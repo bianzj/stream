062e3bf7fb544957768aee457526a47bb3d1e56a
bianzj <jiankegai@163.com>
2025-10-05 19:56:18 +0200

    make basic processes

---
+++ satellite_imaging_simulation/main.h
@@ -0,0 +1,16 @@
+//
+// Created by tirtlab on 25-10-5.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
---
+++ scene_imaging_simulation/main.h
@@ -0,0 +1,8 @@
+//
+// Created by tirtlab on 25-10-5.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
